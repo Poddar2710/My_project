b5ef8b819c850ef648b51c763cb346d96b0e055e
Poddar2710 <arpitakumari2303@gmail.com>
2026-04-17 12:51:03 +0530

    Replace API key with placeholder
    Updated API key to a placeholder for security.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBr0IWbS_ApWXPoZYnOGV4lo7OStHpC0vk")
+client = genai.Client(api_key="your_API")
 
 response = client.models.generate_content(
     model="gemini-1.5-pro",
